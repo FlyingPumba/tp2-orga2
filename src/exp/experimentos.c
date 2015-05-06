1b6fe0baee33a338a3c625f0e1c2da3eb7e5f929
Martin Jedwabny <martiniedva@gmail.com>
2015-05-05 23:57:45 -0300

    saco la mayoría de los outliers sacando 30 muestras y promediando las 10 del medio

--- src/exp/experimentos.c
+++ src/exp/experimentos.c
@@ -12,9 +12,14 @@
 #include "rdtsc.h"
 
 static const char* files_path = "img/";
+static const void (*func_blur[3])(uint32_t w, uint32_t h, uint8_t* data) = {C_blur, ASM_blur1, ASM_blur2};
+static const void (*func_merge[3])(uint32_t w, uint32_t h, uint8_t* data1, uint8_t* data2, float value) = {C_merge, ASM_merge1, ASM_merge2};
+static const void (*func_hsl[3])(uint32_t w, uint32_t h, uint8_t* data, float hh, float ss, float ll) = {C_hsl, ASM_hsl1, ASM_hsl2};
 
-void execute_exp(char*, char*, FILE*);
-void serialize(){__asm__ __volatile__("cpuid; ");}
+void execute_exp(BMP*, unsigned long*);
+void print_results(FILE*, unsigned long*, char*, int, int);
+void serialize();
+int longcmp(const void *a, const void *b);
 
 int main(void)
 {
@@ -29,8 +34,29 @@ int main(void)
         char path[64];
         strcpy(path, files_path);
         strcat(path, dir->d_name);
+        BMP* img = bmp_read(path);
         printf("Ejecutando: %s \n", dir->d_name);
-        execute_exp(path, dir->d_name, file);
+        unsigned long tiempos[9][30];
+        for (int i=0; i<30; i++){
+          unsigned long temp[9];
+          BMP* bmp = bmp_copy(img, 1);
+          execute_exp(bmp, temp);
+          for (int j=0; j<9; j++){
+            tiempos[j][i] = temp[j];
+          }
+        }
+        for (int i=0; i<9; i++){
+          qsort(tiempos[i], 30, sizeof(unsigned long), longcmp);
+        }
+        unsigned long res[9];
+        for (int i=0; i<9; i++){
+          res[i] = 0;
+          for (int j=0; j<9; j++){
+            res[i] += tiempos[i][j+10];
+          }
+          res[i] = res[i]/10;
+        }
+        print_results(file, res, dir->d_name, *(bmp_get_w(img)), *(bmp_get_h(img)));
       }
     }
     closedir(d);
@@ -39,15 +65,9 @@ int main(void)
   return(0);
 }
 
-
-static const void (*func_blur[3])(uint32_t w, uint32_t h, uint8_t* data) = {C_blur, ASM_blur1, ASM_blur2};
-static const void (*func_merge[3])(uint32_t w, uint32_t h, uint8_t* data1, uint8_t* data2, float value) = {C_merge, ASM_merge1, ASM_merge2};
-static const void (*func_hsl[3])(uint32_t w, uint32_t h, uint8_t* data, float hh, float ss, float ll) = {C_hsl, ASM_hsl1, ASM_hsl2};
-
-void execute_exp(char* path, char* image_name, FILE *file)
+void execute_exp(BMP* bmp, unsigned long* res)
 {
   // Leo archivo
-  BMP* bmp = bmp_read(path);
   if(bmp==0) {return;}
   uint8_t* data = bmp_get_data(bmp);
   uint32_t h = *(bmp_get_h(bmp));
@@ -67,8 +87,6 @@ void execute_exp(char* path, char* image_name, FILE *file)
   // Me quedan los datos en w,h,data1,data2
   
   // Tests ------------------------------------------
-
-  unsigned long res[9];
 
   // Blur
   for (int i=0; i<3; i++){
@@ -102,13 +120,6 @@ void execute_exp(char* path, char* image_name, FILE *file)
 
   // End Tests ------------------------------------------
 
-  // Print file
-  fprintf(file, "%s %d %d ", image_name, w, h);
-  for(int i=0;i<9;i++) {
-    fprintf(file, "%ld ", res[i]);
-  }
-  fprintf(file, "\n");
-
   // Libero memoria
   if(*(bmp_get_bitcount(bmp)) == 24) {
     to24(w,h,data1,data);
@@ -117,4 +128,23 @@ void execute_exp(char* path, char* image_name, FILE *file)
     free(data2);
   }
   bmp_delete(bmp);
+}
+
+void print_results(FILE* file, unsigned long* res, char* image_name, int w, int h)
+{
+  fprintf(file, "%s %d %d ", image_name, w, h);
+  for(int i=0;i<9;i++) {  
+    fprintf(file, "%ld ", res[i]);
+  }
+  fprintf(file, "\n");
+}
+
+int longcmp(const void *a, const void *b)
+{
+  return (*(const unsigned long *)(a) < *(const unsigned long *)(b)) ? -1 : (*(const unsigned long *)(a) > *(const unsigned long *)(b));
+}
+
+void serialize()
+{
+  __asm__ __volatile__("cpuid; ");
 }
